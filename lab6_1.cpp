758b730c29f2a0a2c72ed7ac4c23ae41c55aa44a
jiraphat <jiraphat42242@gmail.com>
2022-12-09 16:17:10 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,22 @@
 using namespace std;
 
 int main(){
+int number = 1 ,Even = 0, Odd = 0;
+    while(number != 0){
     cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    cin >> number;
+
+    if(number % 2 == 0){
+        Even = Even + 1;
+    }
+    else{
+        Odd = Odd + 1;
+    }
+}
+
+
+
+    cout << "#Even numbers = " << Even - 1 << "\n";
+    cout << "#Odd numbers = " << Odd;
     return 0;
 }
